3bd963fc92ae28c68ec2c0ebdc2c8414347c8023
Eric Lew <eric@its-be-studio.com>
2024-11-10 12:05:53 +0000

    Further enhancements

--- src/components/Video/VideoContainer.tsx
+++ src/components/Video/VideoContainer.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 import { useGetAnnotations } from "../../hooks/useGetAnnotations";
 import { VideoPlayer } from "./VideoPlayer";
 import { Annotations } from "./Annotations";
@@ -7,7 +7,14 @@ export const VideoContainer = () => {
   const { isLoading, error, annotations } = useGetAnnotations();
 
   return (
-    <Box height="100%" padding="2rem" overflowY="scroll">
+    <Box height="100%" padding="2rem">
+      <Heading
+        fontSize={{ base: "1rem", sm: "1rem", md: "1.2rem" }}
+        paddingBottom="0.25rem"
+      >
+        Experiment 1
+      </Heading>
+
       <VideoPlayer
         annotations={annotations}
         isLoadingAnnotations={isLoading}
